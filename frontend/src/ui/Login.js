ee9c3a6b1e9bc08f4911ce6f7fa57255d3e02a06
Maciej_Trzasalski <mtrzsalski@gmail.com>
2021-04-07 21:18:53 +0200

    added basic logic to login page

--- frontend/src/ui/Login.js
+++ frontend/src/ui/Login.js
@@ -1,17 +1,39 @@
-import "./Home.css";
+import "./Login.css";
+import { useState } from "react";
+const axios = require('axios').default;
 
-const Login = () => (
-  <div className="Home">
-    <div className="Login">
-      <p>Login:</p>
-      <input className="LoginBox"></input>
-      <p>Hasło:</p>
-      <input className="LoginBox"></input>
-      <a href="/" className="LoginButton">
+const Login = () => {
+    const [login, changeLogin] = useState("");
+    const [email, changeEmail] = useState("");
+    const [password, changePassword] = useState("");
+    const [error, changeError] = useState(false);
+
+    const handleChangeLogin = (login) => changeLogin(login);
+    const handleChangePassword = (password) => changePassword(password);
+
+    const handleClick = () => {
+        axios.get('http://localhost:8080/', {login: login, email: email, password: password}).then((resp) => {
+            if (resp.status === 200) 
+        })
+    }
+
+    return (
+    <div className="LoginContainer">
+
+      <label className='Label'>Login<br/>
+        <input className="LoginBox" onChange={(e) => handleChangeLogin(e.target.value)}></input>
+      </label>
+
+      <label className='Label'>Hasło<br/>
+        <input className="LoginBox" onChange={(e) => handleChangePassword(e.target.value)}></input>
+      </label>
+
+        {error && <div className="Error">Zły login lub hasło.</div>}
+
+      <button className="LoginButton" onClick={handleClick}>
         Zaloguj
-      </a>
-    </div>
-  </div>
-);
+      </button>
+  </div>)
+  };
 
 export default Login;
