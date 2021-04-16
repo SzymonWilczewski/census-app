e49a768b8cf9f150023a0b8ccd18eea8f416dac6
filip wozniak <filip.wozniak99@gmail.com>
2021-04-16 16:03:52 +0200

    ZPI added homepage and login design

--- frontend/src/ui/Home.js
+++ frontend/src/ui/Home.js
@@ -2,15 +2,22 @@ import "./Home.css";
 
 const Home = () => (
   <div className="Home">
+    <div className="HomeContainer">
+      <p className="Writing">Narodowy Spis Powszechny</p>
+      <div className="Statistics">
+        <a href="/statistics" className="LoginButton">
+          Statystyki
+        </a>
+        <p>Zobacz szczegółowe statystyki dotyczące Narodowego Spisu Powszechnego Ludności i Mieszkań 2021.</p>
+      </div>
+    </div>
     <div className="Login">
-      <a href="/login" className="LoginButton">
+      <p className="AdminLogging">Logowanie dla Administratora</p>
+      <a href="/login" className="LoginButton" id="secondVariantButton">
         Zaloguj
       </a>
-      <a href="/statistics" className="LoginButton">
-        Statystyki
-      </a>
+      <img className="nspLogo" src="https://spis.gov.pl/wp-content/uploads/2021/01/cropped-logo-nsp.png" alt="nsp-logo"/>
     </div>
-    <div className="Search"></div>
   </div>
 );
 
