105ac20d4a711d11ecae565a33296ebdf99f240b
Kamila <kamkopiczko@gmail.com>
2021-04-21 16:53:39 +0200

    ZPI-55 legenda po polsku

--- frontend/src/ui/UnemploymentChart.js
+++ frontend/src/ui/UnemploymentChart.js
@@ -6,7 +6,7 @@ function UnemploymentChart({ data }) {
     <div>
       <h3>Poziom bezrobocia w Polsce: </h3>
       <BarChart width={600} height={340} data={data}>
-        <Bar dataKey="unemployment" fill="#82ca9d" />
+        <Bar name="bezrobocie" dataKey="unemployment" fill="#82ca9d" />
         <XAxis dataKey="name" />
         <YAxis />
         <Legend />
